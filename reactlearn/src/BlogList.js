d39e7e64c540dca8364187f65a66333a209ad237
nikita <git@github.com:Nikitak211/temp-node.js.git>
2021-10-31 22:44:42 +0200

    learning react

---
+++ reactlearn/src/BlogList.js
@@ -0,0 +1,16 @@
+const BlogList = ({blogs, title, handleDelete}) => {
+    return ( 
+        <div className="blog-list">
+            <h2 className="list-title">{title}</h2>
+             {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}>Delete blog</button>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
